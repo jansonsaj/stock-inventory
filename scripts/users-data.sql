a1f66a4069961adfd2260b3818d23df130986e17
Andris <jansonsa@coventry.ac.uk>
2020-11-17 18:51:39 +0000

    Store the password's hash not plain-text
    Updated database insert statements to store the hashed password instead
    of the plain-text password

--- scripts/users-data.sql
+++ scripts/users-data.sql
@@ -1,9 +1,9 @@
 -- Insert test data into users table
 INSERT INTO users(user, pass, email)
-    VALUES("bemployee1", "p455w0rd", "jansonsaj@gmail.com");
+    VALUES("bemployee1", "$2b$10$qlonuqwEIx4UCUZcmRO04.GueAQA8W52QUTsmTNoBC/03D3vdgYye", "jansonsaj@gmail.com");
 INSERT INTO users(user, pass, email)
-    VALUES("employee2", "p455w0rd", "jansonsaj@gmail.com");
+    VALUES("employee2", "$2b$10$qlonuqwEIx4UCUZcmRO04.GueAQA8W52QUTsmTNoBC/03D3vdgYye", "jansonsaj@gmail.com");
 INSERT INTO users(user, pass, email)
-    VALUES("employee1", "p455w0rd", "jansonsaj@gmail.com");
+    VALUES("employee1", "$2b$10$qlonuqwEIx4UCUZcmRO04.GueAQA8W52QUTsmTNoBC/03D3vdgYye", "jansonsaj@gmail.com");
 INSERT INTO users(user, pass, email)
-    VALUES("andris", "p455w0rd", "jansonsaj@gmail.com");
+    VALUES("andris", "$2b$10$qlonuqwEIx4UCUZcmRO04.GueAQA8W52QUTsmTNoBC/03D3vdgYye", "jansonsaj@gmail.com");
